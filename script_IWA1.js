46ab312adec2199f6d54ebfe486a56a336b0d0e9
Shabbeer Petersen <shabbeerexilepetersen@gmail.com>
2023-07-31 14:11:17 +0200

    modified script_IWA1.

--- script_IWA1.js
+++ script_IWA1.js
@@ -1,34 +1,33 @@
-MAX_NUMBER=10
-MIN_NUMBER=0
+MAX_NUMBER = 10;
+MIN_NUMBER = 0;
 
-const number = document.querySelector('[data-key="number"]')
-const subtract = document.querySelector('[data-key="subtract"]')
-const add = document.querySelector('[data-key="add"]')
+const number = document.querySelector('[data-key="number"]');
+const subtract = document.querySelector('[data-key="subtract"]');
+const add = document.querySelector('[data-key="add"]');
 
-const subtractHandler=() =>{
-    const newValue =parseInt(number.value)-1
-    number.value= newValue
+const subtractHandler = () => {
+  const newValue = parseInt(number.value) - 1;
+  number.value = newValue;
 
-    if(add.disabled === true){
-        add.disabled=false
-    }
-    if(number.value <=MIN_NUMBER){
-        subtract.disabled=true
-    }
-}
+  if (add.disabled === true) {
+    add.disabled = false;
+  }
+  if (number.value <= MIN_NUMBER) {
+    subtract.disabled = true;
+  }
+};
 
-const addHandler=() =>{
-    const newValue =parseInt(number.value) +1
-    number.value= newValue
+const addHandler = () => {
+  const newValue = parseInt(number.value) + 1;
+  number.value = newValue;
 
-    if(subtract.disabled === true){
-        subtract.disabled=false
-    }
-    if(number.value >=MAX_NUMBER){
-        add.disabled=true
-    }
-}
+  if (subtract.disabled === true) {
+    subtract.disabled = false;
+  }
+  if (number.value >= MAX_NUMBER) {
+    add.disabled = true;
+  }
+};
 
-
-subtract.addEventListener('click',subtractHandler)
-add.addEventListener('click',addHandler)
+subtract.addEventListener("click", subtractHandler);
+add.addEventListener("click", addHandler);
