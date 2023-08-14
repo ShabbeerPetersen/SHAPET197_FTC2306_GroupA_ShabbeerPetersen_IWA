02e3aae2c2d1fad40f5a5c99c83c1d8a8ea57974
Shabbeer Petersen <shabbeerexilepetersen@gmail.com>
2023-08-14 10:43:14 +0200

    added index.html files for both challenges

--- SHAPET197_FTC2306_GroupA_ShabbeerPetersen_IWA5/challenge_1/script.js
+++ SHAPET197_FTC2306_GroupA_ShabbeerPetersen_IWA5/challenge_1/script.js
@@ -4,16 +4,16 @@ const BANNED_WARNING =
   "Unfortunately we do not ship to your country of residence";
 
 // User info
-const location = "RSA"; // Country, 'RSA' for South Africa
+const _area = "RSA"; // Country, 'RSA' for South Africa
 const currency = "R"; // Currency, 'R' for South African Rand
 const customers = 1; // Number of customers
 
 let shipping = 0; // defining shipping
 
 // Check where the user is located
-if (location === "RSA") {
+if (_area === "RSA") {
   shipping = 400; // RSA shipping cost
-} else if (location === "NAM") {
+} else if (_area === "NAM") {
   shipping = 600; // NAM shipping cost
 } else {
   shipping = 800; // Other countries' shipping cost
@@ -31,7 +31,7 @@ const totalCost = shoes + toys + shirts + batteries + pens;
 
 // Apply free shipping if conditions are met
 if (totalCost >= 1000 && customers === 1) {
-  if (location === "RSA" || location === "NAM") {
+  if (_area === "RSA" || _area === "NAM") {
     shipping = 0;
   }
 }
@@ -39,7 +39,7 @@ if (totalCost >= 1000 && customers === 1) {
 // Check if you need to show any warnings or prices
 if (shipping === 0 && customers !== 1) {
   console.log(FREE_WARNING);
-} else if (location === "NK") {
+} else if (_area === "NK") {
   console.log(BANNED_WARNING);
 } else {
   console.log("Price:", currency, totalCost + shipping);
