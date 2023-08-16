3206c852d560eb82a1d7e24c9f9cda703cc9d368
Shabbeer Petersen <shabbeerexilepetersen@gmail.com>
2023-08-16 12:09:37 +0200

    modified IWA 7 challenge 3 and commit IWA9

--- SHAPET197_FTC2306_GroupA_ShabbeerPetersen_IWA7/challenge_3/script.js
+++ SHAPET197_FTC2306_GroupA_ShabbeerPetersen_IWA7/challenge_3/script.js
@@ -10,14 +10,14 @@ const divider = "----------------------------------";
 
 // Only change below this line
 
-const leo_owed = parseFloat(leoBalance * -1).toFixed(2);
-const sarah_owed = parseFloat(sarahBalance * -1).toFixed(2);
-const leo = `${leoName} ${leoSurname.trimEnd()} (Owed R${leo_owed})`;
-const sarah = `${sarahName.trimEnd()} ${sarahSurname} (Owed R${sarah_owed})`;
-const total = `Total amount owed: ${parseFloat(leo_owed + sarah_owed).toFixed(
+const leo_owed = parseFloat(leoBalance * -1);
+const sarah_owed = parseFloat(sarahBalance * -1);
+const leo = `${leoName} ${leoSurname.trimEnd()} (Owed R${leo_owed.toFixed(2)})`;
+const sarah = `${sarahName.trimEnd()} ${sarahSurname} (Owed R${sarah_owed.toFixed(
   2
-)}`;
-
+)})`;
+const owed = parseFloat(leo_owed) + sarah_owed;
+const total = `Total amount owed: ${owed}`;
 const result = `${leo}
 ${sarah}
 
