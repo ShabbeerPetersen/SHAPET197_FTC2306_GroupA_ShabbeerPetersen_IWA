b18061d955377a2f78601ce13be8885df6d21689
Shabbeer Petersen <shabbeerexilepetersen@gmail.com>
2023-08-22 16:48:06 +0200

    using forloop to iterate over the amount of orders

--- SHAPET197_FTC2306_GroupA_ShabbeerPetersen_IWA11/challenge_1/script.js
+++ SHAPET197_FTC2306_GroupA_ShabbeerPetersen_IWA11/challenge_1/script.js
@@ -1,47 +1,17 @@
-// Choosing elements for Order 1
-const order1Root = document.querySelector('[data-key="order1"]');
-const order1Biscuits = order1Root.querySelector(".biscuits .count");
-const order1Donuts = order1Root.querySelector(".donuts .count");
-const order1Pancakes = order1Root.querySelector(".pancakes .count");
-const order1Status = order1Root.querySelector(".status dd");
+// Iterate over each order section
+for (let orderNumber = 1; orderNumber <= 3; orderNumber++) {
+  const orderRoot = document.querySelector(`[data-key="order${orderNumber}"]`);
+  const orderBiscuits = orderRoot.querySelector(".biscuits .count");
+  const orderDonuts = orderRoot.querySelector(".donuts .count");
+  const orderPancakes = orderRoot.querySelector(".pancakes .count");
+  const orderStatus = orderRoot.querySelector(".status dd");
 
-// Updating values for Order 1
-order1Biscuits.textContent = order1Root.getAttribute("data-biscuits");
-order1Donuts.textContent = order1Root.getAttribute("data-donuts");
-order1Pancakes.textContent = order1Root.getAttribute("data-pancakes");
-order1Status.textContent =
-  order1Root.getAttribute("data-delivered") === "true"
-    ? "Delivered"
-    : "Pending";
-
-// Choosing elements for Order 2
-const order2Root = document.querySelector('[data-key="order2"]');
-const order2Biscuits = order2Root.querySelector(".biscuits .count");
-const order2Donuts = order2Root.querySelector(".donuts .count");
-const order2Pancakes = order2Root.querySelector(".pancakes .count");
-const order2Status = order2Root.querySelector(".status dd");
-
-// Updating values for Order 2
-order2Biscuits.textContent = order2Root.getAttribute("data-biscuits");
-order2Donuts.textContent = order2Root.getAttribute("data-donuts");
-order2Pancakes.textContent = order2Root.getAttribute("data-pancakes");
-order2Status.textContent =
-  order2Root.getAttribute("data-delivered") === "true"
-    ? "Delivered"
-    : "Pending";
-
-// Choosing elements for Order 3
-const order3Root = document.querySelector('[data-key="order3"]');
-const order3Biscuits = order3Root.querySelector(".biscuits .count");
-const order3Donuts = order3Root.querySelector(".donuts .count");
-const order3Pancakes = order3Root.querySelector(".pancakes .count");
-const order3Status = order3Root.querySelector(".status dd");
-
-// Updating values for Order 3
-order3Biscuits.textContent = order3Root.getAttribute("data-biscuits");
-order3Donuts.textContent = order3Root.getAttribute("data-donuts");
-order3Pancakes.textContent = order3Root.getAttribute("data-pancakes");
-order3Status.textContent =
-  order3Root.getAttribute("data-delivered") === "true"
-    ? "Delivered"
-    : "Pending";
+  // Updating values for the current order
+  orderBiscuits.textContent = orderRoot.getAttribute("data-biscuits");
+  orderDonuts.textContent = orderRoot.getAttribute("data-donuts");
+  orderPancakes.textContent = orderRoot.getAttribute("data-pancakes");
+  orderStatus.textContent =
+    orderRoot.getAttribute("data-delivered") === "true"
+      ? "Delivered"
+      : "Pending";
+}
