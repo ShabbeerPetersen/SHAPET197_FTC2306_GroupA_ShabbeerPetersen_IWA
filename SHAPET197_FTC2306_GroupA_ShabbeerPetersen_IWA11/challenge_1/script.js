3184e3adb0cb0712700196768cc42c4d74be89fb
Shabbeer Petersen <shabbeerexilepetersen@gmail.com>
2023-08-22 22:26:00 +0200

    Added comments

--- SHAPET197_FTC2306_GroupA_ShabbeerPetersen_IWA11/challenge_1/script.js
+++ SHAPET197_FTC2306_GroupA_ShabbeerPetersen_IWA11/challenge_1/script.js
@@ -1,17 +1,17 @@
-// Iterate over each order section
+// Iterate over each order instead of hard coding individual orders.
 for (let orderNumber = 1; orderNumber <= 3; orderNumber++) {
-  const orderRoot = document.querySelector(`[data-key="order${orderNumber}"]`);
-  const orderBiscuits = orderRoot.querySelector(".biscuits .count");
-  const orderDonuts = orderRoot.querySelector(".donuts .count");
-  const orderPancakes = orderRoot.querySelector(".pancakes .count");
-  const orderStatus = orderRoot.querySelector(".status dd");
+  const order_root = document.querySelector(`[data-key="order${orderNumber}"]`);
+  const order_biscuits = order_root.querySelector(".biscuits .count");
+  const order_donuts = order_root.querySelector(".donuts .count");
+  const order_pancakes = order_root.querySelector(".pancakes .count");
+  const order_status = order_root.querySelector(".status dd");
 
   // Updating values for the current order
-  orderBiscuits.textContent = orderRoot.getAttribute("data-biscuits");
-  orderDonuts.textContent = orderRoot.getAttribute("data-donuts");
-  orderPancakes.textContent = orderRoot.getAttribute("data-pancakes");
-  orderStatus.textContent =
-    orderRoot.getAttribute("data-delivered") === "true"
+  order_biscuits.textContent = order_root.getAttribute("data-biscuits");
+  order_donuts.textContent = order_root.getAttribute("data-donuts");
+  order_pancakes.textContent = order_root.getAttribute("data-pancakes");
+  order_status.textContent =
+    order_root.getAttribute("data-delivered") === "true"
       ? "Delivered"
-      : "Pending";
+      : "Pending"; // shorthand if-else
 }
