225e1ea66418ced408fae91ce293e32c86fbbe7c
Shabbeer Petersen <shabbeerexilepetersen@gmail.com>
2023-09-06 08:36:45 +0200

    added comments and edited scripts.js

--- IWA18/challenge_1/scripts.js
+++ IWA18/challenge_1/scripts.js
@@ -1,21 +1,24 @@
-//scripts.js
+import {
+  TABLES,
+  COLUMNS,
+  state,
+  createOrderData,
+  updateDragging,
+} from "./data.js";
+import {
+  createOrderHtml,
+  html,
+  updateDraggingHtml,
+  moveToColumn,
+} from "./view.js";
 
-/**
- * A handler that fires when a user drags over any element inside a column. In
- * order to determine which column the user is dragging over the entire event
- * bubble path is checked with `event.path` (or `event.composedPath()` for
- * browsers that don't support `event.path`). The bubbling path is looped over
- * until an element with a `data-area` attribute is found. Once found both the
- * active dragging column is set in the `state` object in "data.js" and the HTML
- * is updated to reflect the new column.
- *
- * @param {Event} event
- */
+// Event handler for when a user drags over an element inside a column
 const handleDragOver = (event) => {
   event.preventDefault();
   const path = event.path || event.composedPath();
   let column = null;
 
+  // Loop through path to find column hovered
   for (const element of path) {
     const { area } = element.dataset;
     if (area) {
@@ -29,14 +32,109 @@ const handleDragOver = (event) => {
   updateDraggingHtml({ over: column });
 };
 
-const handleDragStart = (event) => {};
-const handleDragEnd = (event) => {};
-const handleHelpToggle = (event) => {};
-const handleAddToggle = (event) => {};
-const handleAddSubmit = (event) => {};
-const handleEditToggle = (event) => {};
-const handleEditSubmit = (event) => {};
-const handleDelete = (event) => {};
+let beingDragged;
+let id;
+
+const handleDragStart = (event) => {
+  // Get the element being dragged
+  beingDragged = event.target.closest(".order");
+  state.dragging.source = state.dragging.over;
+  id = beingDragged.dataset.id;
+};
+
+const handleDragEnd = (event) => {
+  event.preventDefault();
+  const move = state.dragging.over;
+
+  // Move the dragged element to the new column
+  moveToColumn(id, move);
+  updateDraggingHtml({ over: move }); // changed 'column' to 'move'
+};
+
+// Bring up help window
+const handleHelpToggle = (event) => {
+  const { target } = event;
+  if (target == html.other.help) {
+    html.help.overlay.showModal();
+  }
+  if (target == html.help.cancel) {
+    html.help.overlay.close();
+  }
+};
+
+// Brings up add order window
+const handleAddToggle = (event) => {
+  const { target } = event;
+  if (target == html.other.add) {
+    html.add.overlay.showModal();
+  }
+  if (target == html.add.cancel) {
+    html.add.overlay.close();
+  }
+};
+
+const handleAddSubmit = (event) => {
+  event.preventDefault();
+  const title = event.target.elements.title.value;
+  const table = event.target.elements.table.value;
+
+  // Create order data and add it to the state
+  const order = createOrderData({ title, table, column: COLUMNS[0] });
+  state.orders[order.id] = order;
+
+  // Create HTML for the order and append it to the column container
+  const element = createOrderHtml(order);
+  const columnContainer = document.querySelector(
+    `[data-column="${COLUMNS[0]}"]`
+  );
+  columnContainer.appendChild(element);
+
+  html.add.overlay.close();
+};
+
+// Bring up edit order window
+const handleEditToggle = (event) => {
+  const { target } = event;
+  const editOpen = document.querySelector(".order");
+  console.log(target);
+  if (target == editOpen) {
+    html.edit.overlay.showModal();
+  }
+  if (target == html.edit.cancel) {
+    html.edit.overlay.close();
+  }
+};
+// Edit existing order
+const handleEditSubmit = (event) => {
+  event.preventDefault();
+  const order = document.querySelector(".order");
+  order.remove();
+  const data = {
+    title: html.edit.title.value,
+    table: html.edit.table.value,
+    column: html.edit.column.value,
+  };
+
+  // Create order data and HTML, then append to column
+  const orderData = createOrderData(data);
+  const orderHtml = createOrderHtml(orderData);
+  const area = document.querySelector(`[data-area="${data.column}"]`);
+  const column = area.querySelector(`[data-column="${data.column}"]`);
+  column.appendChild(orderHtml);
+
+  html.edit.overlay.close();
+};
+
+// for deleting order
+const handleDelete = (event) => {
+  const { target } = event;
+  if (target == html.edit.delete) {
+    document.querySelector(".order").remove();
+  }
+  html.edit.overlay.close();
+};
+
+// Event listeners for all actions
 
 html.add.cancel.addEventListener("click", handleAddToggle);
 html.other.add.addEventListener("click", handleAddToggle);
@@ -49,6 +147,8 @@ html.edit.delete.addEventListener("click", handleDelete);
 
 html.help.cancel.addEventListener("click", handleHelpToggle);
 html.other.help.addEventListener("click", handleHelpToggle);
+
+// Event listeners for dragging
 
 for (const htmlColumn of Object.values(html.columns)) {
   htmlColumn.addEventListener("dragstart", handleDragStart);
